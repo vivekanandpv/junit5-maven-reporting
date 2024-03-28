79d71516492bcabc273a13ab13e7120d1b4d6955
Vivekanand P V <vivekanandpv@outlook.com>
2024-03-28 17:14:24 +0530

    gson api

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,15 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		Book originalBook = new Book(2, "Learning Java", 1232, 3344.5);
+		Gson gson = new Gson();
+
+		// Serialization: Object -> String/byte[]
+		String jsonBook = gson.toJson(originalBook);
+
+		//	Deserialization: String/byte[] -> Object
+		Book bookFromJson = gson.fromJson(jsonBook, Book.class);
 	}
 
 }
